2a30d327f1aa00dbf2818bc2a037b41575eb4c40
pramod <pramodsomanks@gmail.com>
2024-04-28 14:29:19 +0530

    Update-Searchform-Css

--- obj/Debug/net8.0/JokesWebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/JokesWebApp.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JokesWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d65fead00afc9b539cb7a752163e2adef308973c")]
 [assembly: System.Reflection.AssemblyProductAttribute("JokesWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JokesWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
